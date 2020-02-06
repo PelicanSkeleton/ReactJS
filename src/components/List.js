3072619b4795a54504e9463fb5f7ce76a55e46c0
PelicanSkeleton <sgphillips80@gmail.com>
2020-02-05 20:48:23 -0500

    Activity 13 complete.

---
+++ src/components/List.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function List(props) {
+  const listItems = props.groceries.map(grocery =>(
+    <li key={grocery.id}>{grocery.name}</li>
+  ))
+  return (
+    <ul className="list-group">
+      {listItems}
+    </ul>
+  );
+}
+
+export default List;
