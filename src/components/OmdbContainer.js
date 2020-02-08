d468016a270d4fcbe3af503c640b3c3e8fc7a395
PelicanSkeleton <sgphillips80@gmail.com>
2020-02-08 11:21:33 -0500

    Activity 20 complete.

--- src/components/OmdbContainer.js
+++ src/components/OmdbContainer.js
@@ -13,11 +13,27 @@ class OmdbContainer extends Component {
     search: ""
   };
 
+  componentDidMount(){
+    this.searchMovies("The Matrix");
+  }
   searchMovies = query => {
     API.search(query)
       .then(res => this.setState({ result: res.data }))
       .catch(err => console.log(err));
   };
+
+  handleInputChange = event => {
+    const { value } = event.target;
+
+    this.setState({
+      search: value
+    });
+  }
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    this.searchMovies(this.state.search);
+  }
 
   render() {
     return (
