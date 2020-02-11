7d81bf262538609ecb164b29fbaef1b773fb6744
PelicanSkeleton <sgphillips80@gmail.com>
2020-02-10 20:57:58 -0500

    Start activity 6 week 20.

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,23 +1,25 @@
 import React, { useState, useEffect } from "react";
-import API from "../../utils/API";
 import Container from "../../components/Container";
 import SearchForm from "../../components/SearchForm";
 import SearchResults from "../../components/SearchResults";
 import Alert from "../../components/Alert";
+import ArticleContext from "../../utils/ArticleContext";
+import API from "../../utils/API";
 
 function Search() {
+  const [articleState, setArticleState] = useState({
+    title: "",
+    description: "",
+    url: ""
+  });
+
   const [search, setSearch] = useState("Wikipedia");
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [url, setUrl] = useState("");
   const [error, setError] = useState("");
 
-
   // When the component mounts, update the title to be Wikipedia Searcher
-  // componentDidMount() {
-  //   document.title = "Wikipedia Searcher";
-
   useEffect(() => {
+    document.title = "Wikipedia Searcher";
+
     if (!search) {
       return;
     }
@@ -30,34 +32,41 @@ function Search() {
         if (res.data.status === "error") {
           throw new Error(res.data.message);
         }
-        setTitle(res.data[1]);
-        setDescription(res.data[2][0]);
-        setUrl(res.data[3][0]);
+        setArticleState({
+          title: res.data[1],
+          description: res.data[2][0],
+          url: res.data[3][0]
+        });
       })
       .catch(err => setError(err));
   }, [search]);
-
 
   const handleInputChange = event => {
     setSearch(event.target.value);
   };
 
-  return (
-    <div>
-      <Container style={{ minHeight: "100vh" }}>
-        <h1 className="text-center">Search For Anything on Wikipedia</h1>
-        <Alert type="danger" style={{ opacity: error ? 1 : 0, marginBottom: 10 }}>
-          {error}
-        </Alert>
-        <SearchForm
-          handleInputChange={handleInputChange}
-          results={search}
-        />
-        <SearchResults title={title} description={description} url={url} />
-      </Container>
-    </div>
-  );
-};
+  const handleFormSubmit = event => {
+    event.preventDefault();
+  };
 
+  return (
+    <ArticleContext.Provider value={articleState}>
+      <div>
+        <Container style={{ minHeight: "100vh" }}>
+          <h1 className="text-center">Search For Anything on Wikipedia</h1>
+          <Alert type="danger" style={{ opacity: error ? 1 : 0, marginBottom: 10 }}>
+            {error}
+          </Alert>
+          <SearchForm
+            handleFormSubmit={handleFormSubmit}
+            handleInputChange={handleInputChange}
+            results={search}
+          />
+          <SearchResults />
+        </Container>
+      </div>
+    </ArticleContext.Provider>
+  );
+}
 
 export default Search;
